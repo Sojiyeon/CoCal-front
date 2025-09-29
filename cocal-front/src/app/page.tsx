12566cab5061af5ff587f5165b33b752f07db529
Sojiyeon <jyso0529@gmail.com>
2025-09-29 20:57:28 +0900

    vercel 테스트

--- cocal-front/src/app/page.tsx
+++ cocal-front/src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            다현&지연
           </li>
         </ol>
 
