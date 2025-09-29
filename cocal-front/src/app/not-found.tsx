68845631583a9ff419f946b19eb915ef3083b1ac
kimmmddh <kdh040825@naver.com>
2025-09-29 20:32:26 +0900

    수정1

--- cocal-front/src/app/not-found.tsx
+++ cocal-front/src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
         <html>
         <body>
         <h1>페이지를 찾을 수 없습니다 😢</h1>
-        <p>요청하신 페이지가 존재하지 않거나 이동되었을 수 있습니다.</p>
+        <p>요청하신 페이지가 존재하지 않거나 이동되었을44 수 있습니다.</p>
         </body>
         </html>
 
