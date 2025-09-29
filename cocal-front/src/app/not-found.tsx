f02c35840eed2a0e862be62d4b54c3e12353eabb
kimmmddh <kdh040825@naver.com>
2025-09-29 20:19:17 +0900

    404페이지 수정

--- cocal-front/src/app/not-found.tsx
+++ cocal-front/src/app/not-found.tsx
@@ -1,1 +1,13 @@
 // 404 페이지
+// app/not-found.tsx
+
+export default function NotFound() {
+    return (
+        <html>
+        <body>
+        <h1>페이지를 찾을 수 없습니다 😢</h1>
+        <p>요청하신 페이지가 존재하지 않거나 이동되었을 수 있습니다.</p>
+        </body>
+        </html>
+    );
+}
