ea16bf229a9cc8c808eeb03ea12230869955ba63
Sojiyeon <jyso0529@gmail.com>
2025-10-13 00:14:16 +0900

    API 수정

--- cocal-front/src/app/dashboard/layout.tsx
+++ cocal-front/src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 // app/dashboard/layout.tsx
-import Dashboard from '@/components/dashboard/Dashboard';
+// import Dashboard from '@/components/dashboard/Dashboard';
 import { UserProvider } from '@/components/modals/ProfileSettingModal';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
