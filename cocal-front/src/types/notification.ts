5dfa2dde9547ff2496f2bbe55b3ac353659c95f1
dodo5517 <kh9015548@gmail.com>
2025-10-23 17:53:18 +0900

    feat: NotificationItem interface 분리

---
+++ cocal-front/src/types/notification.ts
@@ -0,0 +1,10 @@
+export interface NotificationItem {
+    id: number;
+    userId: number;
+    type: string;
+    referenceId: number; // 관련 리소스 ID
+    title: string;
+    message: string;
+    isRead: boolean;
+    createdAt: string;
+}
